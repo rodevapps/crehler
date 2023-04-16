ce4ef1a691bfb54f6090a9a14e8fa7e4c9fffd35
Rafal Opiela <opiela@tlen.pl>
2023-04-17 01:00:43 +0200

    Plugin new version

--- backend/custom/plugins/CrehlerExample/src/CrehlerExample.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace CrehlerExample;
-
-use Shopware\Core\Framework\Plugin;
-
-class CrehlerExample extends Plugin
-{
-}
--- backend/custom/plugins/CrehlerExample/src/Resources/app/administration/src/main.js
+++
@@ -1,6 +0,0 @@
-
-import template from './extension/sw-product-settings-form/sw-product-settings-form.html.twig';
-
-Shopware.Component.override('sw-product-settings-form', {
-    template
-});
